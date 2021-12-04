0d5aab40e936be66e7ae2e5d75d17481babf4105
Francis Fueconcillo <francisfueconcillo@gmail.com>
2021-12-05 03:36:56 +0900

    Render root-config outside of  nextjs

---
+++ pages/api/import-map.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  imports: object
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  const imports:Data = {
+    imports: {
+      "single-spa": "https://cdn.jsdelivr.net/npm/single-spa@5.9.0/lib/system/single-spa.min.js",
+      "@single-spa/welcome": "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js",
+      
+    }
+  }
+
+  res.status(200).json(imports)
+}
